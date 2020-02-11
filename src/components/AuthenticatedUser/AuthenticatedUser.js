10969762faf10d56810bb58cd88946b67c3d89bd
Jaman Dedy <jeandedieuam@gmail.com>
2020-02-11 17:00:54 +0200

    protectiong pages

--- src/components/AuthenticatedUser/AuthenticatedUser.js
+++ src/components/AuthenticatedUser/AuthenticatedUser.js
@@ -2,22 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import * as actions from '../../store/actions/index';
+import Aux from '../../hoc/Aux/Aux';
 import classes from './AuthenticatedUser.module.scss';
 import userAvatar from '../../assets/images/avatar.jpg';
 
 class AuthenticatedUser extends Component {
   componentWillMount() {
     this.props.onGetUser();
-    if (this.props.error) {
-      this.props.onSetRedirectPath();
-    }
   }
   render() {
-    const redirectPath = <Redirect to={this.props.redirectPath} />;
     const { user } = this.props.currentUser;
-    return (
+    const currentUser = (
       <div className={classes.User}>
-        {redirectPath}
         <Link to="/profile">
           <img src={user.avatar} alt="" />
         </Link>
@@ -42,6 +38,8 @@ class AuthenticatedUser extends Component {
         </div>
       </div>
     );
+    const redirectPath = <Redirect to="/" />;
+    return <Aux>{this.props.error ? redirectPath : currentUser}</Aux>;
   }
 }
 
@@ -49,15 +47,13 @@ const mapStateToProps = state => {
   return {
     loading: state.currentUser.loading,
     error: state.currentUser.error,
-    currentUser: state.currentUser,
-    redirectPath: state.login.authRedirect || state.register.authRedirectPath
+    currentUser: state.currentUser
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onGetUser: () => dispatch(actions.fetchCurrentUser()),
-    onSetRedirectPath: () => dispatch(actions.setAuthRedirectPath('/login'))
+    onGetUser: () => dispatch(actions.fetchCurrentUser())
   };
 };
 
