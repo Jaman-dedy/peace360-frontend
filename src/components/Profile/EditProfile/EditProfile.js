30a489469efab9a27c0ede7d66dbef8e0b880810
MBenedicte <mbenedicte20@gmail.com>
2020-02-13 17:34:30 +0200

    Add more field in edit Profile page

--- src/components/Profile/EditProfile/EditProfile.js
+++ src/components/Profile/EditProfile/EditProfile.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 import './EditProfile.scss';
 import Layout from '../../Menu/Toolbar/Toolbar';
 import avatar from '../../../assets/images/avatar.jpg';
-
+import { NavLink } from 'react-router-dom';
 class EditProfile extends Component {
+  state = {
+    displayMore: true,
+    displayExperience: true,
+    displayEducation: true,
+    displaySocial: true
+  };
+  onDisplayMore = () => {};
+  onDisplayExperience = () => {};
+  onDisplayEducation = () => {};
+  onDisplaySocial = () => {};
   render() {
     return (
       <div>
@@ -12,13 +23,15 @@ class EditProfile extends Component {
             <Layout></Layout>
           </div>
           <div className='editContent'>
-            <div className='boxContent back'>
-              <i class='fas fa-chevron-left'></i>
-            </div>
+            <NavLink to='/profile'>
+              <div className='boxContent back'>
+                <i class='fas fa-chevron-left'></i>
+              </div>
+            </NavLink>
+
             <div className='edit_container boxContent'>
               <div className=''>
                 <div className='title main-color center'>Edit your Profile</div>
-
                 <form action=''>
                   <div className='form'>
                     <div className='row image'>
@@ -33,13 +46,53 @@ class EditProfile extends Component {
                     <div className='row'>
                       <div className='box'>
                         <fieldset>
-                          <legend>Edit your username</legend>
+                          <legend>Username</legend>
                           <input type='text' placeholder='Bene' />
                         </fieldset>
                       </div>
                       <div className='box'>
                         <fieldset>
-                          <legend> Edit your Email address</legend>
+                          <legend>Email address</legend>
+                          <input
+                            type='text'
+                            placeholder='example@example.com'
+                          />
+                        </fieldset>
+                      </div>
+                    </div>
+                    <div
+                      className='font-color more-about-user'
+                      onClick={this.onDisplayMore}
+                    >
+                      More about you
+                    </div>
+                    <div className='row'>
+                      <div className='box'>
+                        <fieldset>
+                          <legend>Company</legend>
+                          <input type='text' placeholder='Bene' />
+                        </fieldset>
+                      </div>
+                      <div className='box'>
+                        <fieldset>
+                          <legend> Website</legend>
+                          <input
+                            type='text'
+                            placeholder='example@example.com'
+                          />
+                        </fieldset>
+                      </div>
+                    </div>
+                    <div className='row'>
+                      <div className='box'>
+                        <fieldset>
+                          <legend>Address</legend>
+                          <input type='text' placeholder='Bene' />
+                        </fieldset>
+                      </div>
+                      <div className='box'>
+                        <fieldset>
+                          <legend>Skills</legend>
                           <input
                             type='text'
                             placeholder='example@example.com'
@@ -49,23 +102,129 @@ class EditProfile extends Component {
                     </div>
                     <div className='row'>
                       <fieldset>
-                        <legend>Add your bio</legend>
-                        <textarea
-                          name='textarea'
-                          id='textarea'
-                          cols='90'
-                          rows='7'
-                          placeholder='Add your bio'
-                        ></textarea>
+                        <legend>Your bio</legend>
+                        <TextareaAutosize style={{ width: '100%' }} />
+                      </fieldset>
+                    </div>
+                    <div className='row font-color'>
+                      <fieldset>
+                        <legend>Your experience</legend>
+                        <div className='row'>
+                          <div className='box'>
+                            <label htmlFor='position'>Position</label>
+                            <input type='text' placeholder='Your position' />
+                          </div>
+                          <div className='box'>
+                            <label htmlFor='company'>Company</label>
+                            <input type='text' placeholder='Your position' />
+                          </div>
+                        </div>
+                        <div className='row'>
+                          <div className='box'>
+                            <label htmlFor='Address'>Address</label>
+                            <input type='text' placeholder='Your position' />
+                          </div>
+                          <div className='box'>
+                            <label htmlFor='company'>Company</label>
+                            <input type='text' placeholder='Your position' />
+                          </div>
+                        </div>
+                        <div className='row'>
+                          <div className='time_box'>
+                            <label htmlFor='Address'>From</label>
+                            <input type='date' placeholder='Your position' />
+                          </div>
+                          <div className='time_box'>
+                            <label htmlFor='company'>To</label>
+                            <input type='date' placeholder='Your position' />
+                          </div>
+                          <div className='time_box'>
+                            <label htmlFor='company'>Now</label>
+                            <input type='radio' placeholder='Your position' />
+                          </div>
+                        </div>
+                        <div className='row text_area'>
+                          <label htmlFor='company'>Job Description</label>
+                          <TextareaAutosize />
+                        </div>
+                      </fieldset>
+                    </div>
+
+                    <div className='row font-color'>
+                      <fieldset>
+                        <legend>Your Education</legend>
+                        <div className='row'>
+                          <div className='box'>
+                            <label htmlFor='school'>School</label>
+                            <input type='text' placeholder='School' />
+                          </div>
+                          <div className='box'>
+                            <label htmlFor='degree'>Degree</label>
+                            <input type='text' placeholder='Your degree' />
+                          </div>
+                        </div>
+                        <div className='row'>
+                          <div className='box'>
+                            <label htmlFor='Address'>Address</label>
+                            <input type='text' placeholder='Your position' />
+                          </div>
+                          <div className='box'>
+                            <label htmlFor='company'>Company</label>
+                            <input type='text' placeholder='Your position' />
+                          </div>
+                        </div>
+                        <div className='row'>
+                          <div className='time_box'>
+                            <label htmlFor='Address'>From</label>
+                            <input type='date' placeholder='Your position' />
+                          </div>
+                          <div className='time_box'>
+                            <label htmlFor='company'>To</label>
+                            <input type='date' placeholder='Your position' />
+                          </div>
+                          <div className='time_box'>
+                            <label htmlFor='company'>Now</label>
+                            <input type='radio' placeholder='Your position' />
+                          </div>
+                        </div>
+                        <div className='row text_area'>
+                          <label htmlFor='company'>School Description</label>
+                          <TextareaAutosize />
+                        </div>
+                      </fieldset>
+                    </div>
+                    <div className='row font-color'>
+                      <fieldset>
+                        <legend>Social media</legend>
+                        <div className='row social'>
+                          <label htmlFor='linkedin'>Youtube</label>
+                          <input type='checkbox' />
+                          <input type='text' className='link' />
+                        </div>
+                        <div className='row social'>
+                          <label htmlFor='twitter'>Twitter</label>
+                          <input type='checkbox' />
+                          <input type='text' className='link' />
+                        </div>
+                        <div className='row social'>
+                          <label htmlFor='facebook'>Facebook</label>
+                          <input type='checkbox' />
+                          <input type='text' className='link' />
+                        </div>
+                        <div className='row social'>
+                          <label htmlFor='Linkedin'>Linkedin</label>
+                          <input type='checkbox' />
+                          <input type='text' className='link' />
+                        </div>
+                        <div className='row social'>
+                          <label htmlFor='Instagram'>Instagram</label>
+                          <input type='checkbox' />
+                          <input type='text' className='link' />
+                        </div>
                       </fieldset>
                     </div>
                     <div className='btn'>
                       <button>Submit</button>
-                    </div>
-                    <div className='text font-color'>
-                      By submitting this form you agree our terms. See our{' '}
-                      <span className='primary-color'>privacy policy</span> to
-                      learn about how we use your information.
                     </div>
                   </div>
                 </form>
